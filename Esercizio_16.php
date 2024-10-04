a513ec4065c4f3056f672bdfd9e4039bcf0d6989
Davide Susini <susini.davide@itismeucci.com>
2024-10-04 08:49:01 +0200

    es finito

--- Esercizio_16.php
+++ Esercizio_16.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $r = 5;
+        $c = 5;
+        echo "<table border = '1' style = 'border-collapse: collapse;'>";
+
+
+        function creaTabella($col, $row) {
+            for ($i=1; $i <= $col; $i++) { 
+                echo "<tr>";
+                for ($j=1; $j <= $row; $j++) { 
+                    echo "<td style = 'padding: 10px '>{$i},{$j}</td>";
+                }
+                echo "</tr>";
+            }
+        }
+
+        creaTabella($c,$r);
+        echo "</table>"
+    ?>
 </body>
 </html>
